21f53bfc884cef349efc3902dd53d77a73d3ca37
Bartłomiej Rasztabiga <bartlomiej.rasztabiga.official@gmail.com>
2018-09-19 18:13:47 +0200

    TODO: FIX weird 404 errors

--- src/app.js
+++ src/app.js
@@ -30,6 +30,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // Routes
 app.use("/", routes)
 
+// FIX weird 404 errors
+
 // Catch 404 and forward to error handler
 app.use((req, res, next) => {
   const err = new Error("Not Found")
